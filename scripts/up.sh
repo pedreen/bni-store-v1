60fc9700ecfaa8ca28d9fb1d417349f014671ca8
alesinicio <alesinicio@gmail.com>
2025-06-03 19:24:34 +0000

    First commit

---
+++ scripts/up.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+[[ ! -f "${CURRENT_DIR}/../config/local.xml" ]] && echo "Create <config/local.xml> file before proceeding!" && exit 1
+
+echo "Starting services..."
+docker compose -f "${CURRENT_DIR}/../docker-compose.yml" up -d
+
+echo "Updating permissions for writeable dirs..."
+docker compose -f "${CURRENT_DIR}/../docker-compose.yml" exec leadpay chmod a+w /var/www/html/var/log -R
+docker compose -f "${CURRENT_DIR}/../docker-compose.yml" exec leadpay chmod a+w /var/www/html/var/report -R
+docker compose -f "${CURRENT_DIR}/../docker-compose.yml" exec leadpay chmod a+w /var/www/html/media -R
