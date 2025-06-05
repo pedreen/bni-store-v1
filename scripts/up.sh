72297ad1c2e489feb28389f3d95b255f864e7598
alesinicio <alesinicio@gmail.com>
2025-06-05 13:15:57 +0000

    Fix: adjust 'up' script to use regular docker-compose

--- scripts/up.sh
+++ scripts/up.sh
@@ -5,9 +5,9 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 [[ ! -f "${CURRENT_DIR}/../config/local.xml" ]] && echo "Create <config/local.xml> file before proceeding!" && exit 1
 
 echo "Starting services..."
-docker compose -f "${CURRENT_DIR}/../docker-compose.yml" up -d
+docker-compose -f "${CURRENT_DIR}/../docker-compose.yml" up -d
 
 echo "Updating permissions for writeable dirs..."
-docker compose -f "${CURRENT_DIR}/../docker-compose.yml" exec leadpay chmod a+w /var/www/html/var/log -R
-docker compose -f "${CURRENT_DIR}/../docker-compose.yml" exec leadpay chmod a+w /var/www/html/var/report -R
-docker compose -f "${CURRENT_DIR}/../docker-compose.yml" exec leadpay chmod a+w /var/www/html/media -R
+docker-compose -f "${CURRENT_DIR}/../docker-compose.yml" exec leadpay chmod a+w /var/www/html/var/log -R
+docker-compose -f "${CURRENT_DIR}/../docker-compose.yml" exec leadpay chmod a+w /var/www/html/var/report -R
+docker-compose -f "${CURRENT_DIR}/../docker-compose.yml" exec leadpay chmod a+w /var/www/html/media -R
